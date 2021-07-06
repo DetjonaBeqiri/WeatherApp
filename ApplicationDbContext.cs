311993ebd559aa42543da50b343935ffc93f64ae
DetjonaBeqiri <67808788+DetjonaBeqiri@users.noreply.github.com>
2021-07-06 22:54:13 +0200

    Models

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Models
+{
+    // IdentityDbContext contains all the user tables
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {}
+        public DbSet<User> Users { get; set; }
+    }
+}
